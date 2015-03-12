dfc685a1fa5c67ddd653a2ae5f598f974d46cd5b
Ivan Chasov <chasow@yandex.ru>
2015-03-12 08:14:08 +0300

    Пробуем работать с Plaza II

---
+++ Plaza/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PlazaDll = P2ClientGateMTA;
+
+namespace Plaza
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PlazaDll.IP2Connection connect;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            connect = new PlazaDll.CP2ConnectionClass();
+        }
+    }
+}
